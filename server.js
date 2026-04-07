1e63e78c28413e097aac70b8a683b50198ea30c6
guilhermexmada <guilhermeshimadapereira@gmail.com>
2026-04-07 20:42:05 -0300

    chore : trata erros ao iniciar servidor

--- server.js
+++ server.js
@@ -8,6 +8,36 @@ async function startServer() {
         const server = app.listen(port, () => {
             console.log(`>> Aplicação iniciada ... Servidor rodando em http://localhost:${port}`)
         })
+        // tratamento de erros
+        // erro conhecido
+        server.on('error', (err) => {
+            console.error(`Erro ao iniciar o servidor: ${err}`)
+            process.exit(1)
+        })
+
+        // sem try catch
+        server.on('uncaughtException', (err) => {
+            console.error(`Exceção não capturada: ${err}`)
+            process.exit(1)
+        })
+
+        // promise rejeitada
+        process.on('unhandledRejection', (reason) => {
+            console.error('Rejeição não tratada:', reason);
+            process.exit(1);
+        });
+
+        // grateful shutdown
+        process.on('SIGTERM', async () => {
+            console.log('Encerrando servidor...');
+
+            await sequelize.close()
+
+            server.close(() => {
+                console.log('Servidor encerrado corretamente');
+                process.exit(0);
+            });
+        });
     } catch (error) {
         console.error('>> Falha ao iniciar a aplicação: ', error)
         process.exit(1)
