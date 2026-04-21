d181d3a90310f68a22b5eafa6fa8ea3276df268a
Shimada <guilhermeshimadapereira@gmail.com>
2026-04-21 15:10:35 -0300

    chore : cria model para classificação de imagens processadas

---
+++ src/models/Classificacao.js
@@ -0,0 +1,33 @@
+import { classToInvokable } from 'sequelize/lib/utils'
+import connection from '../config/sequelize-config.js'
+import { DataTypes } from 'sequelize'
+
+const Classificacao = connection.define('classificacoes', {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
+    tempo_execucao: {
+        type: DataTypes.INTEGER,
+    },
+    classe:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    confianca: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    modelo_cnn: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},
+    {
+        tableName: 'classificacoes',
+        timestamps: true
+    }
+)
+
+export default Classificacao
