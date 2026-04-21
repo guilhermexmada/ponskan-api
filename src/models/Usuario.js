6b95fed65f72e0b2e5caea802fc65fd53c10a55f
Shimada <guilhermeshimadapereira@gmail.com>
2026-04-21 14:44:18 -0300

    chore : cria model para usuários

---
+++ src/models/Usuario.js
@@ -0,0 +1,53 @@
+import connection from '../config/sequelize-config.js'
+import { DataTypes } from 'sequelize'
+
+const Usuario = connection.define('usuarios', {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    senha: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    telefone: {
+        type: DataTypes.INTEGER
+    },
+    data_nascimento: {
+        type: DataTypes.DATE
+    },
+    tipo_acesso: {
+        type: DataTypes.ENUM('produtor', 'estudante'),
+        defaultValue: 'produtor',
+        allowNull: false
+    },
+    endereco: {
+        type: DataTypes.STRING
+    },
+    cnpj: {
+        type: DataTypes.INTEGER
+    },
+    faculdade: {
+        type: DataTypes.STRING
+    },
+    curso: {
+        type: DataTypes.STRING
+    }
+},
+    {
+        tableName: 'usuarios',
+        timestamps: true
+    }
+)
+
+export default Usuario
