658e29d39f8c3d5ee56a56b35bb5dc8f811e286f
guilhermexmada <guilhermeshimadapereira@gmail.com>
2026-04-28 19:39:37 -0300

    fix : adiciona preload do dotenv.config() ao script de start do servidor

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 import express from 'express'
-import dotenv from 'dotenv'
 import healthRoutes from './src/routes/healthRoutes.js'
+import userRoutes from './src/routes/userRoutes.js'
 import errorMiddleware from './src/middlewares/errorMiddleware.js'
 
-dotenv.config()
 const app = express()
 
 // configurações express
@@ -16,6 +15,7 @@ app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
 app.use('/', healthRoutes)
+app.use('/', userRoutes)
 
 // define middleware como última camada de tratamento de erros
 app.use(errorMiddleware)
