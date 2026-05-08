c9b0e639c02b9b22cbf88834adb878b14ef32e0f
guilhermexmada <guilhermeshimadapereira@gmail.com>
2026-05-07 22:57:50 -0300

    feature : implementa pipelines p/ simular data augmentation, normalização e classificação

--- src/pipelines/sharpPipeline.js
+++ src/pipelines/sharpPipeline.js
@@ -31,6 +31,68 @@ class SharpPipeline {
         const processedBuffer = pipeline.toBuffer()
         return processedBuffer
     }
+    async simulateTraining(buffer, originalName, analysisId) {
+        // simula data augmentation
+        const variations = [
+            // original padronizada
+            sharp(buffer).resize(224, 224).rotate(),
+
+            // com espelhamento
+            sharp(buffer).resize(224, 224).flip().rotate(),
+
+            // matiz alterada
+            sharp(buffer).resize(224, 224).modulate({ hue: 10, brightness: 0.9 }),
+
+            // rotação aleatória
+            sharp(buffer).resize(224, 224).rotate(45)
+        ]
+        // simula normalização
+        const finalImages = await Promise.all(variations.map(v =>
+            v.grayscale()
+                .normalise()
+                .toBuffer()
+        ))
+
+        return finalImages
+    }
+    async simulateClassification(buffers) {
+        const probabilities = []
+        for (const buffer of buffers) {
+            // obtém pixels brutos
+            const { data, info } = await sharp(buffer)
+                .removeAlpha() // remove possível transparência
+                .toColorspace('srgb') // garante 3 canais
+                .raw()
+                .toBuffer({ resolveWithObject: true }) // entrega {data: buffer, info: metadados}
+
+            let count = 0
+            const totalPixels = info.width * info.height
+
+            // analisa canais RGB pulando de 3 em 3 bytes
+            for (let i = 0; i < data.length; i += 3) {
+                const r = data[i]
+                const g = data[i + 1]
+                const b = data[i + 2]
+
+                // regra simulada: conta pixels necróticos (baixos valores em todos os canais)
+                if (r < 30 && g < 30 && b < 30) {
+                    count++
+                }
+            }
+
+            // simula probabilidade
+            console.log('total de pixels: ', totalPixels)
+            console.log('pixels escuros: ', count)
+            const density = (count / totalPixels) * 100
+            const prob = Math.min(density / 2, 1) // normaliza entre 0 e 1, considera 5% = alta probabilidade, 1 = trava de segurança (clamping)
+            probabilities.push(prob)
+        }
+
+        // média final (ensemble averaging) das variações de uma imagem
+        const score = probabilities.reduce((a, b) => a + b, 0) / probabilities.length
+
+        return score
+    }
 }
 
 export default new SharpPipeline()
