fa2d834c5380f11970687e1a91c49396cbd842bd
Shimada <guilhermeshimadapereira@gmail.com>
2026-05-10 23:30:19 -0300

    feature : implementa funções utilitárias p/ manipular arquivos de imagens na storage

--- src/utils/storage/storageService.js
+++ src/utils/storage/storageService.js
@@ -14,6 +14,44 @@ class StorageService {
 
         return fullPath
     }
+    async move(fullPath, folder, analysisId, userId) {
+        // consulta data de criação do arquivo
+        const dt = new Date()
+        const month = dt.getMonth() + 1
+        const year = dt.getFullYear()
+        // define caminho de destino
+        const parsedPath = path.parse(fullPath)
+        // caminho relativo dentro do storage
+        const relativeDestination = path.join(
+            'analysis',
+            `${year}`,
+            `${month}`,
+            `${analysisId}`,
+            `${userId}`,
+            parsedPath.base
+        )
+        // caminho absoluto final
+        const fullDestination = path.resolve(
+            'storage',
+            folder,
+            relativeDestination
+        )
+        // cria diretório
+        await fs.mkdir(path.dirname(fullDestination), {
+            recursive: true
+        })
+        // move arquivo
+        await fs.rename(fullPath, fullDestination)
+
+        return fullDestination
+    }
+    async cleanTemp(folder) {
+        const tempDir = path.resolve('storage', 'temp', folder)
+        await fs.rm(tempDir, {
+            recursive: true,
+            force: true
+        })
+    }
 }
 
 export default new StorageService()
