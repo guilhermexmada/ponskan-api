82e1c9cf8375598ed0d47c4bcef70d61d7ff88ca
Shimada <guilhermeshimadapereira@gmail.com>
2026-05-15 10:17:45 -0300

    refactor : retira redundância dos status HTTP nas responses

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -2,7 +2,7 @@ class APIResponse {
     constructor(res, message, statusCode = 200, data, meta) {
         return res.status(statusCode).json({
             success: true,
-            status: statusCode,
+            // status: statusCode,
             message: message,
             data: data,
             meta: meta
