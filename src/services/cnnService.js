54f2238c42c36e10ca90ca65fef4c3200e71e712
guilhermexmada <guilhermeshimadapereira@gmail.com>
2026-05-22 19:51:18 -0300

    chore : corrige cálculo de confiança para classe saúdavel

--- src/services/cnnService.js
+++ src/services/cnnService.js
@@ -14,7 +14,7 @@ class CNNService {
 
             console.log(`>> Iniciando classificação da Análise ${analysisId} com ${data.length} imagens`)
 
-            // gera array com probabilidade de cada imagem
+            // gera array com probabilidade de cada imagem processada
             let scores = [];
             for (const object of data) {
                 const buffer = object.processed.buffer
@@ -23,14 +23,15 @@ class CNNService {
                 scores.push(probability)
             }
 
-            // média final
+            // média final das probabilidades
             const finalScore = scores.reduce((a, b) => a + b, 0) / scores.length
-
-            const confidence = finalScore
-            const preDiagnosis = confidence >= 0.8 ? 'true' : 'false'
+            // define limiar e classe binária
+            const preDiagnosis = finalScore >= 0.8 ? 'true' : 'false'
+            // cálculo da confiança por probabilidade complementar
+            const confidence = preDiagnosis === 'true' ? finalScore : (1 - finalScore)
 
             await new Promise(resolve => setTimeout(resolve, 5000)) // delay artificial
-            console.log(`Existe ${finalScore * 100}% de chance da amostra ser ${preDiagnosis === 'true' ? 'infectada' : 'saudável'}`)
+            console.log(`Existe ${confidence * 100}% de chance da amostra ser ${preDiagnosis === 'true' ? 'infectada' : 'saudável'}`)
 
             return {
                 confidence,
